ec0b3191943e8f53e7691ae0e350ec8003caecfa
AboveAcrobat284 <cgumetanavarro@gmail.com>
2023-03-28 11:26:53 -0600

    sdo commit

--- src/main/java/com/basedatos/basededatos/models/UserModel.java
+++ src/main/java/com/basedatos/basededatos/models/UserModel.java
@@ -8,11 +8,22 @@ public class UserModel {
     @Column(name = "id",updatable = false, nullable = false,  unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "last_name")
-    private String lastName;
 
     @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "e_mail")
+    private String eMail;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
 
 
     public long getId() {
@@ -23,14 +34,6 @@ public class UserModel {
         this.id = id;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getFirstName() {
         return firstName;
     }
@@ -38,5 +41,32 @@ public class UserModel {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
+
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {return userName;}
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserPassword() {return userPassword;}
+
+
 }
